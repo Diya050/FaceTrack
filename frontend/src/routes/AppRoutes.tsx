f9e894f3c6e3973ae6a38d3c5a3a42741bfe1e39
Dhruvit Garathiya <dhruvitgarthiya@gmail.com>
2026-02-21 19:40:26 +0530

    added help-center page and created route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,6 +5,7 @@ import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
 import PendingApproval from "../pages/auth/PendingApproval";
 import MeetOurTeam from "../pages/user/MeetOurTeam";
+import HelpCenterPage from "../pages/user/HelpCenterPage";
 
 export default function AppRoutes() {
   return (
@@ -15,6 +16,7 @@ export default function AppRoutes() {
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/pending-approval" element={<PendingApproval />} />
       <Route path="/team" element={<MeetOurTeam />} />
+      <Route path="/help-center" element={<HelpCenterPage/>}/>
     </Routes>
   );
 }
