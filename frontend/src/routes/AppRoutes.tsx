08b08e11bbff8b83326bc1209bb31ef05f520a42
Diya Baweja <13diyabaweja11a@gmail.com>
2026-02-21 23:27:56 +0530

    feat: add features page with responsive design

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,6 +5,8 @@ import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
 import PendingApproval from "../pages/auth/PendingApproval";
 import MeetOurTeam from "../pages/user/MeetOurTeam";
+import Features from "../pages/public/Features";
+
 
 export default function AppRoutes() {
   return (
@@ -15,6 +17,8 @@ export default function AppRoutes() {
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/pending-approval" element={<PendingApproval />} />
       <Route path="/team" element={<MeetOurTeam />} />
+      <Route path="/features" element={<Features />} />
+
     </Routes>
   );
 }
