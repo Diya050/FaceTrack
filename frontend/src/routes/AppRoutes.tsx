7ac145ae20586a2cd4c730af670b3c001f6f9df4
Komal Sharma <komalsharmaofficial2023@gmail.com>
2026-02-22 14:54:11 +0530

    Routes optimised

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -11,13 +11,13 @@ import Register from "../pages/auth/Register";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
 import PendingApproval from "../pages/auth/PendingApproval";
-import MeetOurTeam from "../pages/user/MeetOurTeam";
+// import MeetOurTeam from "../pages/user/MeetOurTeam";
 import HelpCenterPage from "../pages/user/HelpCenterPage";
 import AdminGuidePage from "../pages/admin/AdminGuidePage";
 import UserGuidePage from "../pages/user/UserGuidePage";
 import TermsOfUsePage from "../pages/TermsOfUse";
-import MeetOurTeam from "../pages/MeetOurTeam";
 import Features from "../pages/public/Features";
+import FAQ from "../pages/public/FAQ/FAQ";
 
 
 export default function AppRoutes() {
@@ -27,36 +27,26 @@ export default function AppRoutes() {
       {/* Public Pages With Header */}
       <Route element={<PublicLayout />}>
         <Route path="/" />
-        <Route path="/features" />
+        <Route path="/features" element={<Features />} />
         <Route path="/contact" />
         <Route path="/how-it-works" />
         <Route path="/help-center" />
         <Route path="/privacy-policy" />
-        <Route path="/terms-of-use" />
+        <Route path="/terms-of-use" element={<TermsOfUsePage/>}/>
         <Route path="/about-technology" />
-        <Route path="/faqs" />
+        <Route path="/faqs" element={<FAQ/>} />
         <Route path="/team" element={<MeetOurTeam />} /> 
+        <Route path="/help-center" element={<HelpCenterPage/>}/>
+        <Route path="/admin-guide" element={<AdminGuidePage/>} />
+        <Route path="/user-guide" element={<UserGuidePage/>}/>
       </Route>
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/pending-approval" element={<PendingApproval />} />
-      <Route path="/team" element={<MeetOurTeam />} />
-      <Route path="/help-center" element={<HelpCenterPage/>}/>
-      <Route path="/adminsguide" element={<AdminGuidePage/>} />
-      <Route path="/usersguide" element={<UserGuidePage/>}/>
-      <Route path="/termsofuse" element={<TermsOfUsePage/>} />
-    </Routes>
-  );
-}
-
-
-      <Route path="/features" element={<Features />} />
-      <Route path="/faqs" element={<FAQ />} />
-      <Route path="/query" element={<QueryForm />} />
-
+      {/* Auth Pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/pending-approval" element={<PendingApproval />} />
     </Routes>
   );
 }
