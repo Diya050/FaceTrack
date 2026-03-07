eb844aeb2c34c9506e58899a0e8b6e49ac6365e6
Pranjal Amulani <pranjalamulani9@gmail.com>
2026-03-07 20:46:03 +0530

    Assign admin status

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, organization, department, profiles, face_enrollment_request
+from app.api.v1.endpoints import auth, organization, department, profiles, face_enrollment_request, roles
 
 api_router = APIRouter()
 
@@ -8,3 +8,4 @@ api_router.include_router(organization.router)
 api_router.include_router(department.router)
 api_router.include_router(profiles.router)
 api_router.include_router(face_enrollment_request.router)
+api_router.include_router(roles.router)
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,6 +4,15 @@ from jose import jwt
 from passlib.context import CryptContext
 from app.core.config import settings
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
+from jose import JWTError
+from app.db.session import get_db
+from app.models.core import User
+
+security = HTTPBearer()
+
 # Setup Bcrypt for password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -38,3 +47,37 @@ def create_access_token(
     }
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: Session = Depends(get_db)
+):
+
+    token = credentials.credentials
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication token"
+            )
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token could not be validated"
+        )
+
+    user = db.query(User).filter(User.user_id == user_id).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    return user
