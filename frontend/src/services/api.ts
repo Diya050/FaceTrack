8d00f8d25b9db2102c91f58779d8104aed44804c
psychopomp7 <mridulhemrajani7@gmail.com>
2026-04-12 22:23:21 +0530

    resolved axios to hosted backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
+  import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-});
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
+  });
 
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
 
-export default instance;
+  export default instance;
